8a16b3c0d96f86de291539eaa1dae541f6a2cf7e
AmaiRana <ARana@softwareinstitute.com>
2021-08-20 12:12:02 +0100

    First commit

---
+++ src/com/company/Grid.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Random;
+//// if this doesnt work, could use Math.random
+
+public class Grid {
+
+    public int width;
+    public int height;
+    public int mineNum;
+    public int flagNum;
+    
+
+    int [][] grid = new int[10][10];
+
+}
